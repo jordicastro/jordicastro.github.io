507d93bbab377c4d3352cd16c7d8ae915b1e915b
Jordi Castro <jordi.castro003@gmail.com>
2024-05-15 17:31:53 -0500

    react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import MainLayout from './layouts/MainLayout'
+import HomePage from './pages/HomePage'
+import ProjectsPage from './pages/ProjectsPage'
+import AboutPage from './pages/AboutPage'
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<MainLayout/>}>
+        <Route index element={<HomePage/>}/>
+        <Route path='projects' element={<ProjectsPage/>}/>
+        <Route path='about' element={<AboutPage/>}/>
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router}/>
 }
 
 export default App
