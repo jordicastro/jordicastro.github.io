b7daaa86e69e77cd8032681f2a3d5f3101ff715f
Jordi Castro <jordi.castro003@gmail.com>
2024-12-11 21:33:03 -0600

    fixed refactor bug

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -1,20 +1,20 @@
 import { useParams } from 'react-router-dom'
 import data from '../data.json'
 import Card from '../components/ProjectCard'
-import { CardType } from '../components/ProjectCards'
+import { ProjectCard } from '../components/ProjectCards'
 
 const ProjectPage = () => {
   const { id } = useParams<{id: string}>()
 
   // map id to project data
-  const project: CardType | null | undefined = id ? data.projects.find(project => (project.id) === parseInt(id)) : null
+  const project: ProjectCard | null | undefined = id ? data.projects.find(project => (project.id) === parseInt(id)) : null
 
 
   return (
     <div className="mt-20" > 
       {project && (project.github ?
-        <Card card={project} buttonText="GIT HUB" link={project.github} inProjectPage={true} /> :
-        <Card card={project} buttonText="GIT HUB" link={`/projects/${project.id}`} inProjectPage={true} />
+        <Card projectCard={project} buttonText="GIT HUB" link={project.github} inProjectPage={true} /> :
+        <Card projectCard={project} buttonText="GIT HUB" link={`/projects/${project.id}`} inProjectPage={true} />
       )}
     </div>
   )
